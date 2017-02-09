9339c21df49189c76a276343788a320107087614
Svetlin Mladenov <svem@gmx.com>
2017-02-08 21:06:06 -0500

    quadratic equation

---
+++ Basic/5.Quadratic equation/README.md
@@ -0,0 +1,5 @@
+# Clock
+Write a script that enters the coefficients a, b and c of a quadratic equation a*x^2 + b*x + c = 0 
+and calculates and prints its real roots. Note that quadratic equations may have 0, 1 or 2 real roots.
+
+# ![Examples](example.png)
--- Basic/5.Quadratic equation/quadratic_equation.js
+++ Basic/5.Quadratic equation/quadratic_equation.js
@@ -1,3 +1,7 @@
+/*
+ * Write a script that enters the coefficients a, b and c of a quadratic equation a*x^2 + b*x + c = 0 and calculates and
+ * prints its real roots. Note that quadratic equations may have 0, 1 or 2 real roots.
+ */
 
 const quadraticEquation = (a, b, c) => {
   let temp = (Math.pow(b, 2) - ( 4 * a * c));
