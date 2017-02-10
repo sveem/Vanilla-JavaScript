21c86551564034b1c0f69492d10836442cfee6b5
Svetlin Mladenov <svem@gmx.com>
2017-02-10 17:59:22 -0500

    eval() solution

--- Basic/9.Calculate expression/calcExpression.js
+++ Basic/9.Calculate expression/calcExpression.js
@@ -3,4 +3,9 @@
  * expression put in the text field and prints it in the paragraph. Link the JS file to the HTML file.
  */
 
- 
+const getInput = () =>{
+  let input = document.getElementById("input").value;
+  let calculate = eval(input);
+  let output = document.getElementById("output").innerHTML = calculate;
+};
+
