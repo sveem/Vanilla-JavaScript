43faf582ad7cab2dfeade76741d30c7e4fe488b1
Svetlin Mladenov <svem@gmx.com>
2017-02-10 16:39:59 -0500

    index.html

---
+++ Basic/9.Calculate expression/calcExpression.js
@@ -0,0 +1,6 @@
+/*
+ * Write a HTML page (with text field, button, and paragraph). Write JS program calcExpression.js that calculates any
+ * expression put in the text field and prints it in the paragraph. Link the JS file to the HTML file.
+ */
+
+ 
