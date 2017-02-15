86c0803e86c53073bcd21f33d2bcd2f28fb6808a
Svetlin Mladenov <svem@gmx.com>
2017-02-14 20:59:43 -0500

    array manipulator

---
+++ Basic/Array manipulator/arrayManipulator.js
@@ -0,0 +1,17 @@
+/*
+ * You are given an array of objects. Your task is to write a JavaScript code that filters out the numbers of that array
+ * (removes all non-number objects). Then you should: 
+ * 1. Find the Min number, 
+ * 2. Find the Max number, 
+ * 3. Find the most frequent number. 
+ * Finally you should print out the numbers you have found and the filtered array sorted in
+ * descending order.
+ */
+
+ let input =  [ 
+   "Pesho", 2, "Gosho", 
+   12, 2, "true", 9, 
+   undefined, 0, "Penka", 
+   { bunniesCount: 10}, 
+   [10, 20, 30, 40] 
+ ];
