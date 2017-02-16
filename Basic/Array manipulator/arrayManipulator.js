fc19475895c629de5cf4294e802d44cba48bc0c0
Svetlin Mladenov <svem@gmx.com>
2017-02-16 18:56:37 -0500

    Array manipulator

--- Basic/Array manipulator/arrayManipulator.js
+++ Basic/Array manipulator/arrayManipulator.js
@@ -15,3 +15,24 @@
    { bunniesCount: 10}, 
    [10, 20, 30, 40] 
  ];
+
+ let filtered, minValue, maxValue, mostFrequent; 
+
+ filtered = input.filter((el) => {
+   return typeof el === "number";
+ })
+ .sort((a, b) => a < b);
+
+ minValue = filtered[filtered.length - 1];
+ maxValue = filtered[0];
+
+ filtered.forEach((el, index, array) => {
+   if (array.indexOf(el) !== index) {
+     mostFrequent = el;
+   }
+ });
+
+ console.log("Min value:", minValue);
+ console.log("Max value:", maxValue);
+ console.log("Most frequent number:", mostFrequent);
+ console.log(filtered);
