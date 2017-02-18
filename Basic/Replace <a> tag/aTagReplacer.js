3b130665eb435e581cb75188d2ec6577ea9a92ff
Svetlin Mladenov <svem@gmx.com>
2017-02-18 17:52:45 -0500

    input

---
+++ Basic/Replace <a> tag/aTagReplacer.js
@@ -0,0 +1,7 @@
+/* 
+ * Write a JavaScript function replaceATag(str) that replaces in a HTML document given as string all the tags <a
+ * href=" ">…</a> with corresponding tags [URL=…]…/URL]. Write JS program aTagReplacer.js that invokes your
+ * function with the sample input data below and prints the output at the console.
+ */
+
+ let input = "<ul><li><a href=http://softuni.bg>SoftUni</a></li></ul>";
