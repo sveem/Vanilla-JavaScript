8eb4d6d21a85c2ee8f2a4a5eef7103fd23f3f5b1
Svetlin Mladenov <svem@gmx.com>
2017-02-19 21:29:24 -0500

    replace <a> tag

--- Basic/Replace <a> tag/aTagReplacer.js
+++ Basic/Replace <a> tag/aTagReplacer.js
@@ -4,4 +4,16 @@
  * function with the sample input data below and prints the output at the console.
  */
 
- let input = "<ul><li><a href=http://softuni.bg>SoftUni</a></li></ul>";
+let input = "<ul> \n <li>\n  <a href=http://softuni.bg>SoftUni</a>\n </li>\n</ul>";
+
+const replaceATag = (str) => {
+  let aTagStart = str.replace("<a", "[URL");
+  let aTagEnd = aTagStart.replace("</a>", "[/URL]");
+  let index = aTagEnd.search(/>\S/);
+  let result = aTagEnd.replace(aTagEnd[index], "]");
+  return result;
+};
+
+console.log(replaceATag(input));
+
+
