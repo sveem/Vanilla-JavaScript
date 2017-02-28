3d1e82b3c578d3e515b1b15789e1733295acd69e
Svetlin Mladenov <svem@gmx.com>
2017-02-28 18:44:08 -0500

    groupBy

---
+++ Fundamentals/Group people/groupBy.js
@@ -0,0 +1,22 @@
+/*
+ * Write a JavaScript function groupBy('criteria') that groups an array of people by age, first or last name. Create a
+ * Person constructor to add every person in the person array. The groupBy('criteria') function must return an object,
+ * with keys – the groups (age, firstName and lastName) and values – arrays with people in this group. Print on the
+ * console every entry of the returned object. Use function overloading (i.e. just one function).
+ * You may need to find how to use constructors.
+*/
+
+
+let people = [
+  new Person('Scott', 'Guthrie', 38),
+  new Person('Scott', 'Johns', 36),
+  new Person('Scott', 'Hanselman', 39),
+  new Person('Jesse', 'Liberty', 57),
+  new Person('Jon', 'Skeet', 38)
+];
+
+const groupBy = (input) => {
+
+};
+
+groupBy('criteria');
