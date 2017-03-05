59055a8c7ba4e1bedcc4c4a3ecabd42836961c41
Svetlin Mladenov <svem@gmx.com>
2017-03-05 18:55:08 -0500

    readMe

---
+++ Fundamentals/Strings's letters organizer/sortString.js
@@ -0,0 +1,13 @@
+/*
+ * Write a JavaScript function sortLetters(string, boolean) that gets as an input a string and a boolean. The function
+ * sorts all letters in the string in alphabetical order and returns the newly formed string. The sorting is ascending if
+ * the boolean is true otherwise the sorting is in descending order.
+ * Note: The sorting is case-insensitive but the output should use the same casing as the input!
+ * Hint: You are allowed to use .sort() function.
+ */
+
+ let input = 'HelloWorld';
+
+ const sortLetters = (string, boolean) => {
+
+ };
