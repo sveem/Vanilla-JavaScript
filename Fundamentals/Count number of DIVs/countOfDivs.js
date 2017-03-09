b3fdee9542176ed18c91b0ebd5c164be629cf462
Svetlin Mladenov <svem@gmx.com>
2017-03-09 17:18:49 -0500

    count number of Divs - readme

---
+++ Fundamentals/Count number of DIVs/countOfDivs.js
@@ -0,0 +1,30 @@
+/*
+ * Write a JavaScript function countDivs(html) to count the number of all DIVs in given HTML fragment passed as
+ * string. Write a JS program countOfDivs.js that invokes your function and prints the output at the console.
+ */
+
+ let input = 
+`<!DOCTYPE html>
+<html>
+<head lang="en">
+	<meta charset="UTF-8">
+	<title>index</title>
+	<script src="/yourScript.js" defer></script>
+</head>
+<body>
+	<div id="outerDiv">
+	<div class="first">
+	<div><div>hello</div></div>
+	</div>
+	<div>hi<div></div></div>
+	<div>I am a div</div>
+</div>
+</body>
+</html>`;
+
+ const countDivs = (html) => {
+
+ };
+
+ console.log(countDivs(input));
+
