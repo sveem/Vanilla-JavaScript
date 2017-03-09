430361e803c9b846a29c3c4a86b668f14281689e
Svetlin Mladenov <svem@gmx.com>
2017-03-08 19:10:21 -0500

    readme

---
+++ Fundamentals/Find youngest person/youngestPerson.js
@@ -0,0 +1,16 @@
+/*
+ * Write a JavaScript function findYoungestPerson(array) that accepts as parameter an array of people, finds the
+ * youngest person that has a smartphone and returns his full name. Write a JS program youngestPerson.js to execute
+ * your function for the below examples and print the result at the console.
+ */
+
+ let people = [
+{ firstname: 'George', lastname: 'Kolev', age: 32, hasSmartphone: false },
+{ firstname: 'Vasil', lastname: 'Kovachev', age: 40, hasSmartphone: true },
+{ firstname: 'Bay', lastname: 'Ivan', age: 81, hasSmartphone: true },
+{ firstname: 'Baba', lastname: 'Ginka', age: 40, hasSmartphone: false }
+ ];
+
+ const findYoungestPerson = (array) => {
+
+ };
