14610bc30c13afc30c83c27c5d74bf69a9e5cd33
Svetlin Mladenov <svem@gmx.com>
2017-03-10 17:50:24 -0500

    working count number of Divs

--- Fundamentals/Count number of DIVs/countOfDivs.js
+++ Fundamentals/Count number of DIVs/countOfDivs.js
@@ -4,26 +4,32 @@
  */
 
  let input = 
-`<!DOCTYPE html>
-<html>
-<head lang="en">
-	<meta charset="UTF-8">
-	<title>index</title>
-	<script src="/yourScript.js" defer></script>
-</head>
-<body>
-	<div id="outerDiv">
-	<div class="first">
-	<div><div>hello</div></div>
-	</div>
-	<div>hi<div></div></div>
-	<div>I am a div</div>
-</div>
-</body>
-</html>`;
+  `<!DOCTYPE html>
+  <html>
+  <head lang="en">
+	  <meta charset="UTF-8">
+	  <title>index</title>
+	  <script src="/yourScript.js" defer></script>
+  </head>
+  <body>
+	  <div id="outerDiv">
+	  <div class="first">
+	  <div><div>hello</div></div>
+	  </div>
+	  <div>hi<div></div></div>
+	  <div>I am a div</div>
+  </div>
+  </body>
+  </html>`;
 
  const countDivs = (html) => {
 
+   let divs = /<div/gi;
+   let result = html
+     .match(divs)
+     .length;
+  
+   return result;
  };
 
  console.log(countDivs(input));
