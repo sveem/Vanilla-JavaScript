fb6379fb01914447e9118ef583282aca7333fa12
Svetlin Mladenov <svem@gmx.com>
2017-03-13 14:00:48 -0400

    deep copy

---
+++ Fundamentals/Deep copy of object/deepCopy.js
@@ -0,0 +1,15 @@
+/*
+ * Write a JavaScript function clone(obj) that accepts as parameter an object of reference type. 
+ * The function should return a deep copy of the object. 
+ * Write a second function compareObjects(obj, objCopy) that compare the two objects by reference (==) and print on the console the output given below. 
+ * Read in Internet about "deep copy" of an object and how to create it.
+ */
+
+const clone = (obj) => {
+  
+};
+
+const compareObjects = (obj, objCopy) => {
+
+};
+
