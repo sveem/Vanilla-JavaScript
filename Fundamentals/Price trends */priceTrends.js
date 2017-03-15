2cad3d7bfde13f03ba2535c854e3b63426a0cd24
Svetlin Mladenov <svem@gmx.com>
2017-03-14 20:51:32 -0400

    price trends

--- Fundamentals/Price trends */priceTrends.js
+++ Fundamentals/Price trends */priceTrends.js
@@ -22,3 +22,39 @@
  * -Allowed working time: 0.2 seconds. Allowed memory: 16 MB.
  */
 
+
+let price1 = ['50', '60'];
+let price2 = [
+  '36.333', 
+  '36.5', 
+  '37.019', 
+  '35.4', 
+  '35', 
+  '35.001', 
+  '36.225'
+];
+
+const priceTrends = (input) => {
+  let temp, currentPrice;
+  let priceArray = input.map(el => Number(el).toFixed(2));
+ 
+  console.log('<table>');
+  console.log('<tr><th>Price</th><th>Trend</th></tr></td></tr>');
+  
+  for (let i = 0; i < priceArray.length; i++) {
+    currentPrice = priceArray[i];
+
+    if (priceArray[i] > priceArray[i - 1]) {
+      temp = 'up.png';
+    } else if (priceArray[i] < priceArray[i - 1]) {
+      temp = 'down.png';
+    } else {
+      temp = 'fixed.png';
+    }
+    console.log(`<tr><td>${currentPrice}</td><td><img src="${temp}"</td></tr>`);
+  }
+  return '</table>';
+};
+
+console.log(priceTrends(price1));
+console.log(priceTrends(price2));
