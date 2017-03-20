f18eb8aa758e4f156ea38528240e19a795374d79
Svetlin Mladenov <svem@gmx.com>
2017-03-20 19:39:21 -0400

    Triangle Area readme

---
+++ Fundamentals/Triangle area/triangleArea.js
@@ -0,0 +1,12 @@
+/*
+ * Write a JS function that calculates a triangle’s area by its 3 sides.
+ * The input comes as array of three string elements, each holding a number, representing one side of a triangle.
+ * The output should be printed to the console.
+ */
+
+ let input = [2, 3.5, 4];
+
+ const triangleArea = (parameters) => {
+   
+ };
+
