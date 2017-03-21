143cf427a08b3585afb79a6bf522224f3b6bbf52
Svetlin Mladenov <svem@gmx.com>
2017-03-21 19:06:38 -0400

    triangle area

--- Fundamentals/Triangle area/triangleArea.js
+++ Fundamentals/Triangle area/triangleArea.js
@@ -8,5 +8,13 @@
 
  const triangleArea = (parameters) => {
    
+   let a = input[0];
+   let b = input[1];
+   let c = input[2];
+
+   let sides = (Number(a) + Number(b) + Number(c)) / 2;
+   let area = Math.sqrt(sides * (sides - a) * (sides - b) * (sides - c));
+   return area;
  };
 
+ console.log(triangleArea(input));
