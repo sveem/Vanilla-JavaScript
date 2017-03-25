79dd718a9b695205ff0252d44006f1cd27c838e4
Svetlin Mladenov <svem@gmx.com>
2017-03-25 18:35:53 -0400

    distance over time readme

---
+++ Fundamentals/Distance over Time/time.js
@@ -0,0 +1,7 @@
+/*
+ * Two objects start from point A and travel in the same direction at constant speeds V1 and V2 for a period T. Write a
+ * JS function that calculates the distance between the two object at the end of the period.
+ * The input comes as array of strings that need to be parsed as numbers. 
+ * The first two elements are the speeds to the two objects in km/h and the third element is the time in seconds.
+ * The output should be printed to the console. Calculate the distance in meters.
+ */
