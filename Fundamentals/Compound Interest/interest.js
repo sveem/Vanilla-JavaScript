f5744508121cbc07f7a19eb4e8e31a68dc4ebdb9
Svetlin Mladenov <svem@gmx.com>
2017-03-26 17:45:43 -0400

    Compound Interest

---
+++ Fundamentals/Compound Interest/interest.js
@@ -0,0 +1,8 @@
+/*
+ * Write a JS function that calculates the total accumulated value for a monetary deposit by given principal sum,
+ * interest rate, compounding frequency and overall length.
+ * The input comes as an array of strings that need to be parsed as a numbers. The first value is the principal sum, the
+ * second is the interest rate in percent, the third is the compounding period in months and the final value is the total
+ * timespan, given in years.
+ * The output should be printed to the console, with two decimal places.
+ */
