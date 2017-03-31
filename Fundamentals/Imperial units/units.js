a3190c0e123c252e7e07fe0177446286ad45d75a
Svetlin Mladenov <svem@gmx.com>
2017-03-30 20:00:30 -0400

    Imperial Units readme

---
+++ Fundamentals/Imperial units/units.js
@@ -0,0 +1,10 @@
+/*
+ * Imperial units are confusing, but still in use in some backwards countries (Myanmar, Liberia and the United States
+ * are the only countries still using them). They are so confusing in fact, that native users struggle to convert between
+ * them. Write a JS function that converts from inches to feet and inches. 
+ * There are 12 inches in a foot. See the example for formatting details.
+ * The input comes as an array of one string element that needs to be parsed as a number.
+ * The output should be printed to the console.
+ */
+
+
