e012ee0b00fc08839df8e1a898401ff93c9f542b
Svetlin Mladenov <svem@gmx.com>
2017-04-04 18:04:02 -0400

    Binary to decimal Readme

---
+++ Fundamentals/Binary to decimal/binaryConvertor.js
@@ -0,0 +1,12 @@
+/*
+ * Write a JS function that reads an 8-bit binary number and converts it to a decimal.
+ * The input comes as an array of one string element, representing a binary number.
+ * The output should be printed to the console.
+ */
+
+ let input1 = ['00001001'];
+ let input2 = ['11110000'];
+
+ const binaryConvertor = (array) => {
+
+ };
