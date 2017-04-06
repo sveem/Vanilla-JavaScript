310d81e207df08e217aeb048be1fcb17cb7849e7
Svetlin Mladenov <svem@gmx.com>
2017-04-05 23:21:59 -0400

    Triangle of stars - Readme

---
+++ Fundamentals/Triangle of stars/triangle.js
@@ -0,0 +1,14 @@
+/*
+ * Write a JS function that outputs a triangle made of stars with variable size, depending on an input parameter. 
+ * Look at the examples to get an idea.
+ * The input comes as an array with one string element in it, which needs to be parsed to a number.
+ * The output is a series of lines printed on the console, forming a triangle of variable size.
+ */
+
+ let data1 = ['1'];
+ let data2 = ['2'];
+ let data3 = ['5'];
+
+ const triangle = (input) => {
+
+ };
