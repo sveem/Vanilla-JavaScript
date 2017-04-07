3943a86c9a62cdc742a685891549d2676ac3b31f
Svetlin Mladenov <svem@gmx.com>
2017-04-07 13:05:45 -0400

    Functional calculator - Readme

---
+++ Fundamentals/Functional calculator/calculator.js
@@ -0,0 +1,6 @@
+/*
+ * Write a JS program that receives two variables and an operator and performs a calculation between the variables,
+ * using the operator. Store the different functions in variables and pass them to your calculator.
+ * The input comes as an array of three strings – two need to be parsed as numbers, and one is the operator.
+ * The output should be printed on the console.
+ */
