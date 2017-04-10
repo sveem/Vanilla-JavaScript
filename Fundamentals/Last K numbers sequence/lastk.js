f9bd5f8b00241eec2d59e5b01064717f815ec7cb
Svetlin Mladenov <svem@gmx.com>
2017-04-09 22:26:38 -0400

    Last K Numbers Sequence Readme

---
+++ Fundamentals/Last K numbers sequence/lastk.js
@@ -0,0 +1,9 @@
+/*
+ * You are given two integers n and k. Write a JS function that generates and prints the following sequence:
+ * - The first element is 1
+ * - Every following element equals the sum of the previous k elements
+ * - The length of the sequence is n elements
+ * The input comes as array of two string elements holding numbers. 
+ * The first element represents the number n, and the second – the number k.
+ * The output is printed on the console on a single line, separated by space.
+ */
