bb6424bf1d9a291c06e5141142a596c10be95a41
Svetlin Mladenov <svem@gmx.com>
2017-04-14 18:27:08 -0400

    Extract Text Readme

---
+++ Fundamentals/Strings and Regular Expressions/Extract Text/extractText.js
@@ -0,0 +1,9 @@
+/*
+ * You will be given a text as a string. 
+ * Write a JS function that extracts and prints only the text that’s surrounded by parentheses.
+ * The input comes as array of a single string element.
+ * The output is printed on the console on a single line, in the form of a comma-separated list.
+ */
+
+
+
