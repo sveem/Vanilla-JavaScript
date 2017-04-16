86f461d360fdc25629fb41e7b733db48acf5046b
Svetlin Mladenov <svem@gmx.com>
2017-04-16 18:20:06 -0400

    extract text

--- Fundamentals/Strings and Regular Expressions/Extract Text/extractText.js
+++ Fundamentals/Strings and Regular Expressions/Extract Text/extractText.js
@@ -6,4 +6,30 @@
  */
 
 
+let input = ['Rakiya (Bulgarian brandy) is self-made liquor (alcoholic drink)'];
 
+const extractText = (text) => {
+
+  let words = [];
+  let leftIndex = -1;
+  text = text.join(' ');
+
+  while (true) {
+    leftIndex = text.indexOf('(', leftIndex);
+    if (leftIndex < 0) {
+      break;
+    }
+
+    let rightIndex = text.indexOf(')', leftIndex + 1);
+    if (rightIndex < 0) {
+      break;
+    }
+
+    words.push(text.substring(leftIndex + 1, rightIndex));
+    leftIndex = rightIndex + 1;
+  }
+
+  return words.join(', ');
+};
+
+console.log(extractText(input));
