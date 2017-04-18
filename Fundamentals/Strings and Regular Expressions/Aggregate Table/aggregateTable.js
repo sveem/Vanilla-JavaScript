75fea27f620400e4f27056ccdfc74dee40b03352
Svetlin Mladenov <svem@gmx.com>
2017-04-17 20:18:54 -0400

    You will be given a list of towns and incomes for each town, formatted in a table, separated by pipes (|). Write a JS

---
+++ Fundamentals/Strings and Regular Expressions/Aggregate Table/aggregateTable.js
@@ -0,0 +1,8 @@
+/*
+ * You will be given a list of towns and incomes for each town, formatted in a table, separated by pipes (|). 
+ * Write a JS function that extracts the names of all towns and produces a sum of the incomes. 
+ * Note that splitting may result in empty string elements and the number of spaces may be different in every table.
+ * The input comes as array of string elements. Each element is one row in a formatted table.
+ * The output is printed on the console on two lines. On the first line, print a comma-separated list of all towns and on
+ * the second, the sum of all incomes.
+ */
