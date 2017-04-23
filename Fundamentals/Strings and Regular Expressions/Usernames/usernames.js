b2b9d7efb4de3c32aa6da4e16ef3e8c6225a15e5
Svetlin Mladenov <svem@gmx.com>
2017-04-23 19:13:21 -0400

    Usernames - Readme

---
+++ Fundamentals/Strings and Regular Expressions/Usernames/usernames.js
@@ -0,0 +1,10 @@
+/*
+ * Write a JS function that parses a list of emails and returns a list of usernames, generated from them. 
+ * Each username is composed from the name of the email address, a period and the first letter of every element in the domain name.
+ * See the examples for more information.
+ * The input comes as array of string elements. Each element is an email address.
+ * The output is printed on the console on a single line as a comma-formatted list.
+ */
+
+let input = ['peshoo@gmail.com', 'todor_43@mail.dir.bg', 'foo@bar.com'];
+
