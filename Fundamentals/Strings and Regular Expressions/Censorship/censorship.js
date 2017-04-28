93c73d2a7b85268e31fb26c7d1e73f98b23dbb46
Svetlin Mladenov <svem@gmx.com>
2017-04-28 16:46:25 -0400

    finished censorship

--- Fundamentals/Strings and Regular Expressions/Censorship/censorship.js
+++ Fundamentals/Strings and Regular Expressions/Censorship/censorship.js
@@ -7,3 +7,21 @@
  * are the strings to be censored.
  * The output is the return value of your functions. Save the censored results in a string and return it.
  */
+
+
+ let input1 = ['roses are red, violets are blue', ', violets are', 'red'];
+ let input2 = ['David Ruben Piqtoukun (born 1950) is an Inuit artist from Paulatuk, Northwest Territories. His output includes sculpture and prints; the sculptural work is innovative in its use of mixed media. His materials and imagery bring together modern and traditional Inuit stylistic elements in a personal vision. An example of this is his work "The Passage of Time" (1999), which portrays a shaman in the form of a salmon moving through a hole in a hand. While shamanic imagery is common in much of Inuit art, the hand in this work is sheet metal, not a traditional material such as walrus ivory, caribou antler or soapstone. Ruben\'s brother, Abraham Apakark Anghik Ruben, is also a sculptor. Fellow Inuit artist Floyd Kuptana learned sculpting techniques as an apprentice to David Ruben.', 'Inuit'];
+
+ const censorship = input => {
+
+   let text = input[0];
+
+   for (let strings of input) {
+     let censored = new RegExp(strings, 'g');
+     text = text.replace(censored, match =>'-'.repeat(Number(match.length)));
+   }
+   return text;
+ };
+
+ console.log(censorship(input1));
+ console.log(censorship(input2));
