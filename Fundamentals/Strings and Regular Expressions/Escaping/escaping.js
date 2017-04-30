37efec847399ff457ee1ef0fae963f9fdffd3e8b
Svetlin Mladenov <svem@gmx.com>
2017-04-29 23:38:57 -0400

    Escaping ReadMe

---
+++ Fundamentals/Strings and Regular Expressions/Escaping/escaping.js
@@ -0,0 +1,9 @@
+/*
+ * You will be given a list of strings, containing user-submitted data. 
+ * Write a JS function that prints an HTML list from
+ * the data. 
+ * The strings, however, may contain special HTML characters, which is an oft-used method for injection ttacks. 
+ * To prevent unwanted behavior or harmful content, all special characters need to be replaced with their
+ * encoded counterparts – they will look the same to the user, but will not pose a security risk. Use the following table
+ * to compose your function:
+ */
