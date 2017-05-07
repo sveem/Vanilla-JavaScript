79e3a248c3c364d1dcde81246dcb83387ed1dd82
Svetlin Mladenov <svem@gmx.com>
2017-05-07 18:50:25 -0400

    Nth Element- ReadMe

---
+++ Fundamentals/Arrays, Matrices, Multi-Dimensional Arrays/Print every N-th Element from an Array/nthElement.js
@@ -0,0 +1,7 @@
+/*
+ * Write a JS function that prints every element of an array, on a given step.
+ * The input comes as array of strings. The last element is N - the step.
+ * The output is every element on the N-th step starting from the first one. 
+ * If the step is “3”, you need to print the 1-st, the 4-th, the 7-th … and so on, until you reach the end of the array. 
+ * The elements must be printed each on a new line.
+ */
