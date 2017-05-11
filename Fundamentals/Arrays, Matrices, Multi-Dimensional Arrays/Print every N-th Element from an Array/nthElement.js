a46e8bd0f625d2c4309c59f874c3e74e66ad1b25
Svetlin Mladenov <svem@gmx.com>
2017-05-10 20:43:49 -0400

    finished N-th element

--- Fundamentals/Arrays, Matrices, Multi-Dimensional Arrays/Print every N-th Element from an Array/nthElement.js
+++ Fundamentals/Arrays, Matrices, Multi-Dimensional Arrays/Print every N-th Element from an Array/nthElement.js
@@ -5,3 +5,27 @@
  * If the step is “3”, you need to print the 1-st, the 4-th, the 7-th … and so on, until you reach the end of the array. 
  * The elements must be printed each on a new line.
  */
+
+let input1 = ['5', '20', '31', '4', '20', '2'];
+let input2 = ['1', '2', '3', '4', '5', '6'];
+let input3 = ['dsa', 'asd', 'test', 'tset', '2'];
+
+const nthElement = array => {
+
+  let result = array[0];
+  let lastElement = Number(array[array.length - 1]);
+
+  for (let i = 1; i < array.length - 1; i++) {
+    if (i % lastElement === 0) {
+      result += '\n' + array[i];
+    }  
+  }
+  return result;
+};
+
+console.log(nthElement(input1));
+//Output: 5, 31, 20
+console.log(nthElement(input2));
+//Output: 1
+console.log(nthElement(input3));
+//Output: dsa, test
