84325a3aa3f3e29a803f9c2c5b12b4b4436c8025
Svetlin Mladenov <svem@gmx.com>
2017-05-21 21:33:23 -0400

    ReadMe

---
+++ Fundamentals/Arrays, Matrices, Multi-Dimensional Arrays/Sort an Array by 2 Criteria/sorting.js
@@ -0,0 +1,7 @@
+/*
+ * Write a JS function that orders a given array of strings, by length in ascending order as primary criteria, and by
+ * alphabetical value in ascending order as second criteria. 
+ * The comparison should be case-insensitive.
+ * The input comes as array of strings.
+ * The output is the ordered array of strings.
+ */
