372da7af9759b250e042c8eca695b4bb2a66a4a4
Svetlin Mladenov <svem@gmx.com>
2017-05-22 22:51:05 -0400

    Finished Sorting

--- Fundamentals/Arrays, Matrices, Multi-Dimensional Arrays/Sort an Array by 2 Criteria/sorting.js
+++ Fundamentals/Arrays, Matrices, Multi-Dimensional Arrays/Sort an Array by 2 Criteria/sorting.js
@@ -5,3 +5,22 @@
  * The input comes as array of strings.
  * The output is the ordered array of strings.
  */
+
+
+let input = ['alpha', 'beta', 'gamma']; 
+let input1 = ['Isacc', 'Theodor', 'Jack', 'Harrison', 'George'];
+let input2 = ['test', 'Deny', 'omen', 'Default'];
+
+const sorting = array => {
+  let result = array
+    .sort()
+    .sort((a, b) => a.length > b.length);
+  return result.join('\n');
+};
+
+console.log(sorting(input));
+//Output: beta, alpha, gamma
+console.log(sorting(input1));
+//Output: Jack, Isacc, George, Theodor, Harrison
+console.log(sorting(input2));
+//Output: Deny, omen, test, Default
