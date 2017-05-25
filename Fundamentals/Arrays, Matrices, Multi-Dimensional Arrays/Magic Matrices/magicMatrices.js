320d3d2fdc9e860c77f5f00aa8b009ac00a6beea
Svetlin Mladenov <svem@gmx.com>
2017-05-24 20:04:43 -0400

    Magic Matrices - ReadMe

---
+++ Fundamentals/Arrays, Matrices, Multi-Dimensional Arrays/Magic Matrices/magicMatrices.js
@@ -0,0 +1,7 @@
+/*
+ * Write a JS function that checks if a given matrix of numbers is magical. 
+ * A matrix is magical if the sums of the cells of every row and every column are equal.
+ * The input comes as array of strings. Each element represents a string of numbers, with spaces between them. 
+ * Parse it into a matrix of numbers, so you can work with it. The input numbers will always be positive.
+ * The output is a Boolean result indicating whether the matrix is magical or not.
+ */
