986cb0872a9b34945ee4f69b619fd88cc4f21a29
Svetlin Mladenov <svem@gmx.com>
2017-06-04 14:12:44 -0400

    finished spiral matrix

--- Fundamentals/Arrays, Matrices, Multi-Dimensional Arrays/Spiral Matrix */spiral.js
+++ Fundamentals/Arrays, Matrices, Multi-Dimensional Arrays/Spiral Matrix */spiral.js
@@ -5,3 +5,61 @@
  * The output is the matrix filled spirally starting from 1. 
  * You need to print every row on a new line, with the cells separated by a space. 
  */
+
+
+ const spiralMatrix = ([input]) => {
+ 
+   let [maxRows, maxCols] = input.split(' ').map(Number);
+   let matrix = [];
+   for (let i = 0; i < maxRows; i++) {
+     matrix.push([]);
+     for (let j = 0; j < maxCols; j++) {
+       matrix[i].push(0);
+     }
+   }
+  
+   let element = 0;
+   let col = -1;
+   let row = 1;
+
+   while (element < maxRows * maxCols) {
+     row--;
+     col++;
+     
+     while (col < maxCols && matrix[row][col] === 0) { 
+       element++;
+       matrix[row][col] = element;
+       col++;
+     }
+     col--;
+     row--;
+       
+     while (row >= 0 && matrix[row][col] === 0) {
+       element++;
+       matrix[row][col] = element;
+       row--;
+     }
+     row++;
+     col--;
+        
+     while (col >= 0 && matrix[row][col] === 0) {
+       element++;
+       matrix[row][col] = element;
+       col--;
+     }
+     row++;
+     col++;
+        
+     while (row < maxRows && matrix[row][col] === 0) {
+       element++;
+       matrix[row][col] = element;
+       row++;
+     }
+   }
+   
+   console.log(matrix.map(row => row.join(' ')).join('\n'));
+ };
+ 
+ spiralMatrix(['3 3']);
+ spiralMatrix(['5 5']);
+
