bc7f244618338bd954ae0ccec8e6ef4851ab7332
Svetlin Mladenov <svem@gmx.com>
2017-06-24 11:51:52 -0400

    Score to HTML readme

---
+++ Fundamentals/Objects, Associative Arrays, Maps, Sets/Score to HTML/score.js
@@ -0,0 +1,9 @@
+/*
+ * You are given a JSON string representing an array of objects, parse the JSON and create a table using the supplied
+ * objects. The table should have 2 columns &quot;name&quot; and &quot;score&quot;, each object in the array will also have these keys.
+ * Any text elements must also be escaped in order to ensure no dangerous code can be passed.
+ * You can either write the HTML escape function yourself or use the one from the Strings and Regular Expressions Lab.
+ * The input comes as an array of strings containing a single string - the array of objects as a JSON.
+ * The output should be printed on the console - a table with 2 columns - &quot;name&quot; and &quot;score&quot;, containing the values
+ * from the objects as rows.
+ */
