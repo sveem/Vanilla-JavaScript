a1c28f57a578c1f73cafa7f7598878157d32099e
Svetlin Mladenov <svem@gmx.com>
2017-06-26 22:45:29 -0400

    Sum by Town - Readme

---
+++ Fundamentals/Objects, Associative Arrays, Maps, Sets/Sum by Town/sumByTown.js
@@ -0,0 +1,12 @@
+/*
+ * You’re tasked with calculating the total sum of income for a number of Towns. You will receive an array of strings
+ * representing towns and their incomes, every even index will be a town and every odd index will be an income
+ * belonging to that town. Create an object that will hold all the towns as keys and their total income (the sum of their
+ * incomes) as values to those keys and print it as a JSON.
+ * The input comes as an array of strings - each even index is the name of a town and each odd index is an income
+ * belonging to that town.
+ * The output should be printed on the console - JSON representation of the object containing all towns and their total
+ * incomes.
+ */
+
+let input = ['Sofia', '20', 'Varna', '3', 'Sofia', '5', 'Varna', '4'];
