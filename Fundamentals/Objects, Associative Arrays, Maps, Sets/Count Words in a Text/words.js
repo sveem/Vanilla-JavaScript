f9f6306835acaf9359a717dba990b782b2793b00
Svetlin Mladenov <svem@gmx.com>
2017-06-30 20:11:58 -0400

    cound of words readme

---
+++ Fundamentals/Objects, Associative Arrays, Maps, Sets/Count Words in a Text/words.js
@@ -0,0 +1,8 @@
+/*
+ * You are tasked to count the number of words in a text using an object as an associative array, any combination of
+ * letters, digits and _ (underscore) should be counted as a word. The words should be stored in the object as
+ * properties - the key being the word and the value being the amount of times the word is contained in the text.
+ * The input comes as an array of strings containing one entry - the text whose words should be counted. The text may
+ * consist of more than one sentence.
+ * The output should be printed on the console - the JSON representation of the object containing the words.
+ */
